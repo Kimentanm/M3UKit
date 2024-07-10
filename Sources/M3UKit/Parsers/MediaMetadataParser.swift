9467c77c48a27de257b0417884bf8fc9063571fd
Kimen <Kimentanm@gmail.com>
2024-07-10 22:14:26 +0800

    Update MediaMetadataParser.swift

--- Sources/M3UKit/Parsers/MediaMetadataParser.swift
+++ Sources/M3UKit/Parsers/MediaMetadataParser.swift
@@ -57,6 +57,7 @@ final class MediaMetadataParser: Parser {
   }
 
   func extractName(_ input: String) -> String {
+    input.replacingOccurrences(of: "#EXTINF:-1,", with: "#EXTINF:-1 ")
     return nameRegex.firstMatch(in: input) ?? ""
   }
 
